3e1d4c34dc822b21f54360fac79ceb81ace24106
Jean Brune <95942357+JeanBrune@users.noreply.github.com>
2022-01-15 23:38:30 -0300

    changed how buttons work for a more intuitive UX

--- js/contador.js
+++ js/contador.js
@@ -20,44 +20,63 @@ function atualizarTimer() {
     }, 1000);
 }
 
+//buscar botões no HTML para usar nas funções
+const btnIniciar = document.getElementById("btn_iniciar")
+const btnPausar = document.querySelector("#btn_pausar")
+const btnZerar = document.querySelector("#btn_zerar")
 
 //criar função de incializar
 function iniciar() {
     //impedir que o botão seja clicado de novo
-    document.querySelector(".iniciar").disabled = true
-    document.querySelector(".pausar").disabled = false
+    //document.querySelector(".iniciar").disabled = true
+    //document.querySelector(".pausar").disabled = false
+    btnIniciar.classList.toggle("escondido")
+    btnPausar.classList.toggle("escondido")
+
+    //impedir que o botão acumule ordens de iniciar, rodando estranho
     //clearTimeout(atualizar)
-    //colocar cor verde
+
+    //colocar cor verde no tempo ativo
     relogio.classList.remove("pausado")
     relogio.classList.add("iniciado")
-    //incrementar ligar o timer
+
+    //incrementar/ligar o timer
     atualizarTimer()
+
     console.log(`contagem iniciada em ${segundos} segundos`)
 }
 
+//criar função de pausar
 function pausar() {
     //impedir que o botão seja clicado de novo
-    document.querySelector(".iniciar").disabled = false
-    document.querySelector(".pausar").disabled = true
-    //pausar o contador
-    clearTimeout(atualizar)
-    //colocar cor vermelha
+    //document.querySelector(".iniciar").disabled = false
+    //document.querySelector(".pausar").disabled = true
+    btnPausar.classList.toggle("escondido")
+    btnIniciar.classList.toggle("escondido")
+
+    //colocar cor laranja no tempo pausado
     relogio.classList.remove("iniciado")
     relogio.classList.add("pausado")
+
+    //pausar o contador
+    clearTimeout(atualizar)
     console.log(`contador pausado em ${segundos} segundos`)
 }
 
+//criar função de resetar
 function zerar() {
-//pausar o contador
-clearTimeout(atualizar)
-relogio.innerHTML = "00:00:00"
-//zerar o contador
-//zerar o elemento html
-segundos = 0
-//retirar as cores
-relogio.classList.remove("iniciado", "pausado")
-//habilitar todos os botões
-document.querySelector(".iniciar").disabled = false
-document.querySelector(".pausar").disabled = true
-console.log(`contador zerado`)
+    //pausar o contador
+    clearTimeout(atualizar)
+    relogio.innerHTML = "00:00:00"
+    //zerar o contador
+    //zerar o elemento html
+    segundos = 0
+    //retirar as cores
+    relogio.classList.remove("iniciado", "pausado")
+    //habilitar todos os botões
+    //document.querySelector(".iniciar").disabled = false
+    //document.querySelector(".pausar").disabled = true
+    btnPausar.classList.add("escondido")
+    btnIniciar.classList.remove("escondido")
+    console.log(`contador zerado`)
 }
